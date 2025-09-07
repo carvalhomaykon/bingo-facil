ec921e488f6803fe7fccd3b0128464449cbaec7c
carvalhomaykon <carvalhomaykon566@gmail.com>
2025-09-07 16:18:17 -0300

    initial config

---
+++ src/main/java/com/bingofacil/bingofacil/BingofacilApplication.java
@@ -0,0 +1,13 @@
+package com.bingofacil.bingofacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingofacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BingofacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bingofacil/bingofacil/BingofacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bingofacil.bingofacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BingofacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
