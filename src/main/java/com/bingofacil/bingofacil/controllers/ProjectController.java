bbe690035310391e814c39a7f88f09f810353eb8
carvalhomaykon <carvalhomaykon566@gmail.com>
2025-09-09 23:29:59 -0300

    Create controlles

---
+++ src/main/java/com/bingofacil/bingofacil/controllers/ProjectController.java
@@ -0,0 +1,32 @@
+package com.bingofacil.bingofacil.controllers;
+
+import com.bingofacil.bingofacil.dtos.project.ProjectDTO;
+import com.bingofacil.bingofacil.model.project.Project;
+import com.bingofacil.bingofacil.services.project.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/projects")
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @PostMapping
+    public ResponseEntity<Project> createProject(@RequestBody ProjectDTO project){
+        Project newProject = projectService.createProject(project);
+        return new ResponseEntity<>(newProject, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Project>> findAllProjects(){
+        List<Project> projects = this.projectService.findAllProjects();
+        return new ResponseEntity<>(projects, HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/bingofacil/bingofacil/controllers/UserController.java
@@ -0,0 +1,37 @@
+package com.bingofacil.bingofacil.controllers;
+
+import com.bingofacil.bingofacil.dtos.project.UserDTO;
+import com.bingofacil.bingofacil.model.user.User;
+import com.bingofacil.bingofacil.services.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserDTO user) {
+        User newUser = this.userService.createUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAllUsers(){
+        List<User> users = this.userService.findAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findUserById(@PathVariable Long id){
+        User user = this.userService.findUserById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+}
